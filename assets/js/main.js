ee02755ae2db95cd6aa5ce3c2a44fcab9ead9339
Ted Krueger <ted.krueger@ritterim.com>
2020-09-25 09:33:50 -0400

    sidebar css and js tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,4 +41,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
       table.className = 'pui-table mb-4';
     });
   };
+
+  // accordion
+  let accordionHeader = document.querySelectorAll('.pui-accordion__header');
+
+  if (accordionHeader) {
+    accordionHeader.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        let accordion = el.parentElement;
+        e.preventDefault();
+        accordion.classList.toggle('active');
+      });
+      el.addEventListener('keydown', function(e) {
+        let accordion = el.parentElement;
+        if (e.keyCode === 13) {
+          e.preventDefault();
+          accordion.classList.toggle('active');
+        }
+      });
+    });
+  }
 });
