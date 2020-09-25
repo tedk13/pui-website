75c3bff45c6521ee28f21ffce6fbdc0aa8a29c70
Ted Krueger <ted.krueger@ritterim.com>
2020-09-25 08:53:45 -0400

    add a bold modifier on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,19 +12,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
       let li = document.createElement('li');
       let a = document.createElement('a');
       a.setAttribute('href', '#'+heading.id);
+      a.className = 'text-base text-light--hover toc-link';
       a.textContent = heading.innerText;
       li.append(a);
       tocList.append(li);
     });
   
     toc.append(tocList);
+
+    const tocLink = document.querySelectorAll('.toc-link');
+
+    if(tocLink) {
+      tocLink.forEach((link) => {
+        link.addEventListener('click', () => {
+          tocLink.forEach((elm) => {
+            elm.classList.remove('text--bold');
+          });
+          link.classList.add('text--bold');
+        });
+      });
+    }    
   }  
 
   const table = main.querySelectorAll('table');
 
   if(table) {
     table.forEach((table) => {
-      table.className = "pui-table mb-4";
+      table.className = 'pui-table mb-4';
     });
   };
 });
