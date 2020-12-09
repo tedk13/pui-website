e4ce0b5fea1a06d078fc9ca6aebf114eedc30487
Ryan Trimble <mrtrimble2@gmail.com>
2020-12-09 12:35:22 -0500

    fixed quotes in js

--- assets/js/skeletons.js
+++ assets/js/skeletons.js
@@ -1,21 +1,21 @@
-const skeletons = document.querySelectorAll(".skeleton");
+const skeletons = document.querySelectorAll('.skeleton');
 
 if (skeletons) {
   skeletons.forEach((skelly) => {
-    let lineCount = parseInt(skelly.getAttribute("data-lines"));
+    let lineCount = parseInt(skelly.getAttribute('data-lines'));
 
-    let animation = skelly.getAttribute("data-animation") === "true";
+    let animation = skelly.getAttribute('data-animation') === 'true';
 
     if (!lineCount && skelly.innerHTML.trim().length == 0) {
       lineCount = 1;
     }
 
     for (i = 0; i < lineCount; i++) {
-      const line = document.createElement("span");
-      line.classList.add("skeleton__line");
+      const line = document.createElement('span');
+      line.classList.add('skeleton__line');
       if (animation == true) {
-        const animationElement = document.createElement("span");
-        animationElement.classList.add("skeleton__animation");
+        const animationElement = document.createElement('span');
+        animationElement.classList.add('skeleton__animation');
         line.appendChild(animationElement);
       }
       skelly.appendChild(line);
